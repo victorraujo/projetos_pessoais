6a7f1ebe4805e792785359d25abc13e57452ce7b
victorraujo <victorrafaeldantas18@gmail.com>
2026-09-06 01:38:56 -0300

    ajeitando codigo

--- calculadora/game.c
+++ calculadora/game.c
@@ -1,91 +1,114 @@
 #include <stdio.h>
-#include <stdlib.h> // malloc end realloc
-#include <string.h> // strlen
-#include <ctype.h> // verificadores
+#include <stdlib.h>  // malloc end realloc
+#include <string.h>  // strlen
+#include <ctype.h>   // verificadores
 #include <stdbool.h> // true or false
-
-
-// SAVE STRING IN HEAP
-// RETURN NULL OR ADDRESS
-char* save_string(void)
-{
-    char text[50000];
-    fgets(text, sizeof(text), stdin);
-
-    size_t size = (text + 1);
-
-    char* text_promt = malloc(size * sizeof(char));
-    
-    if (text_promt ==  NULL) return NULL;
-
-    return text_promt;
-}
-
 
 typedef struct type_text
 {
     char *op;
-    int *number;
-    
- // metadores
-    type_text *inicio;
-    type_text *esquerda;
-    type_text *direita;
+    double *number;
+
+    // metadores
+    struct type_text *inicio;
+    struct type_text *esquerda;
+    struct type_text *direita;
 } type_text;
 
-
-
+voidinserir_no(type_text *usuario, type_text *tmp)
+{
+    if (usuario == NULL)
+    {
+        tmp->inicio = tmp;
+        tmp->direita = NULL;
+        tmp->direita = NULL;
+        tmp->esquerda = NULL;
+        usuario = tmp;
+    }
+    else
+    {
+        tmp->direita = NULL;     // entra dentro do nó e deixa como terminador null
+        tmp->esquerda = usuario; // salva o de tras
+        usuario->direita = tmp;  // apontar pro novo
+        usuario = tmp;
+    }
+}
 int main(void)
 {
-    type_text* usuario = NULL;
-
+    type_text *usuario = NULL;
     int caractere_digitado = 0; // vaeriavel de ajuda
-    while ((caractere_digitado = getchar()) !='\n' && caractere_digitado != EOF)
-    {
-        type_text* tmp = malloc(sizeof(type_text));
-        if (tmp == NULL) return 1;
+    int novo_numero = 0;
+    double acumular = 0;
+    bool tem_numero = false; // flag
 
+    while ((caractere_digitado = getchar()) != '\n' && caractere_digitado != EOF)
+    {
         if (isdigit(caractere_digitado) != 0) // NUMBERS
         {
-            tmp->number = malloc(sizeof(caractere_digitado));
-            if (tmp->number == NULL) return 1;
-   
-            *tmp->number = (caractere_digitado - '0'); //The number zero is worth 48.
-            tmp->op == NULL;
+            int novo_numero = (caractere_digitado - '0'); // The number zero is worth 48.
+
+            acumular   = (acumular * 10) + novo_numero;
+            tem_numero = true;
         }
 
         else if (caractere_digitado == '+' || caractere_digitado == '-' || // OPERACION
-            caractere_digitado == '*' || caractere_digitado == '/' || 
-            caractere_digitado == '(' || caractere_digitado == ')') 
+                 caractere_digitado == '*' || caractere_digitado == '/' ||
+                 caractere_digitado == '(' || caractere_digitado == ')')
         {
+            tem_numero = false;
+
             tmp->op = malloc(sizeof(char));
-            if (tmp->op) return 1;
+            if (tmp->op == NULL)
+                return 1;
 
             *tmp->op = caractere_digitado;
-            tmp->number == NULL;
+            tmp->number = NULL;
         }
 
-        else if(isalpha(caractere_digitado) != 0 || caractere_digitado == ' ')
+        else if (isalpha(caractere_digitado) != 0 || caractere_digitado == ' ')
         {
-            free(tmp);
             continue;
         }
-        
 
-        if (usuario == NULL)
+        if (!tem_numero)
         {
-            tmp->inicio = tmp;
-            tmp->direita = tmp;
-            tmp->direita->direita = NULL;
-            tmp->esquerda = NULL;
-            usuario = tmp;
-        }
-        else
-        {  
-            tmp->direita = NULL;  // entra dentro do nov e deixa como terminador null      
-            usuario->esquerda = usuario; // salva o de tras    
-            usuario->direita = tmp; // aponta pro novo
-            usuario = tmp;
+            type_text *tmp = malloc(sizeof(type_text));
+            if (tmp == NULL)
+                return 1;
+
+            tmp->number = malloc(sizeof(double));
+            tmp->op = malloc(sizeof(char));
+
+            if (tmp->number == NULL && tmp->op == NULL)
+                return 1;
+
+            *tmp->number = acumular;
+            acumular = 0;
+
+            *tmp->op = caractere_digitado;
         }
 
+        if (!tem_numero)
+        {
+            inserir_no(&usuario, &tmp_op);
+        }
+    }
+    type_text *sensor = usuario->inicio;
+    type_text *start_op = NULL;
+    double valor_esquerda;
+    double valor_direta;
+    double resultado;
+
+    while (sensor != NULL)
+    {
+        if (sensor != NULL && sensor->op == '*' || sensor->op == '/')
+        {
+            type_text *inicio_op = sensor;
+            while (inicio_op->op == NULL)
+            {
+
+                inicio_op->direita;
+            }
+        }
+    }
 }
