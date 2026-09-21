75abe7c106fb440170163f66a86e5d735c81ab39
victorraujo <victorrafaeldantas18@gmail.com>
2026-09-21 15:39:19 -0300

    tela de game feita

--- jogo_scape/game.c
+++ jogo_scape/game.c
@@ -4,7 +4,7 @@
 #include <conio.h>
 
 #define ALTURA 20
-#define LARGURA 20
+#define LARGURA 40
 #define LIMITE_TOTAL 20
 
 typedef struct
@@ -29,23 +29,21 @@ typedef struct
 
 char tela[ALTURA][LARGURA];
 
-
 void inicializar()
 {
-    
+
     memset(tela, ' ', sizeof(tela));
 
     usuario.personagem = '@';
     usuario.pontos = 0;
     usuario.y = ALTURA / 2;
-    usuario.x = LARGURA /2;
+    usuario.x = LARGURA / 2;
 
     tela[usuario.y][usuario.x] = '@';
-
 }
 void barreira()
 {
-    for (int barreira_altura = 0; barreira_altura <= ALTURA; barreira_altura++)
+    for (int barreira_altura = 0; barreira_altura < ALTURA; barreira_altura++)
     {
         if (barreira_altura == 0)
         {
@@ -54,20 +52,27 @@ void barreira()
                 tela[barreira_altura][i] = '#';
             }
         }
-        if (barreira_altura == ALTURA)
+        else if (barreira_altura == ALTURA - 1)
         {
             for (int i = 0; i < LARGURA; i++)
             {
                 tela[barreira_altura][i] = '#';
             }
         }
-        printf("\n");
+        else
+        {
+            tela[barreira_altura][0] = '#';
+            tela[barreira_altura][LARGURA - 1] = '#';
+        }
     }
 }
 void imprimirMatriz()
 {
-    for (int i = 0; i < ALTURA; i++) {
-        for (int j = 0; j < LARGURA; j++) {
+    printf("\033[H");
+    for (int i = 0; i < ALTURA; i++)
+    {
+        for (int j = 0; j < LARGURA; j++)
+        {
 
             printf("%c", tela[i][j]);
         }
@@ -86,48 +91,48 @@ int main(void)
     objetivo.x = 7;
     objetivo.personagem = '$';
 
-    while(!gamerOver)
+    while (!gamerOver)
     {
-        printf("\033[2J\033[H");
 
         imprimirMatriz();
 
         char movimento = _getch();
 
+        if (movimento == 224 || movimento == 0)
+        {
+            movimento  = _getch();
+        }
+        // Apaga a posição antiga do jogador
+        tela[usuario.y][usuario.x] = ' ';
+
         switch (movimento)
         {
-        case 80: // cima
-            tela[usuario.y][usuario.x] = ' '; 
-            usuario.y++;
-            tela[usuario.y][usuario.x] = '@'; 
-            
+        case 72: // SETA PARA CIMA (Sobe uma linha no terminal)
+            if (usuario.y > 1)
+                usuario.y--;
             break;
-        case 72: // baixo
-            tela[usuario.y][usuario.x] = ' '; 
-            usuario.y--;
-            tela[usuario.y][usuario.x] = '@'; 
 
+        case 80: // SETA PARA BAIXO (Desce para a linha de baixo)
+            if (usuario.y < ALTURA - 2)
+                usuario.y++;
             break;
-        case 75: // esquerda
-            tela[usuario.y][usuario.x] = ' '; 
-            usuario.x--;
-            tela[usuario.y][usuario.x] = '@'; 
+
+        case 75: // SETA PARA ESQUERDA
+            if (usuario.x > 1)
+                usuario.x--;
             break;
-        case 77: // direita
-            tela[usuario.y][usuario.x] = ' '; 
-            usuario.x++;
-            tela[usuario.y][usuario.x] = '@'; 
 
-        break;
-
-        default:
+        case 77: // SETA PARA DIREITA
+            if (usuario.x < LARGURA - 2)
+                usuario.x++;
             break;
         }
-        if (usuario.y >= 20 && usuario.x >= 20 && usuario.y < 0 && usuario.x < 0)
+        tela[usuario.y][usuario.x] = usuario.personagem;
+        
+        if (usuario.y >= ALTURA - 2 && usuario.x >= LARGURA - 2 && usuario.y < 0 && usuario.x < 0)
         {
             gamerOver == true;
         }
     }
     return 0;
-
 }
