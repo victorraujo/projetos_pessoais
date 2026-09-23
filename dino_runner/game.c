750094b084efd621a656911e12daabd854c677c8
victorraujo <victorrafaeldantas18@gmail.com>
2026-09-23 19:32:12 -0300

    adicionando pulo um pouco mais sufisticado. porém tenho que melhorar a organização que depois irei verificar isso

--- dino_runner/game.c
+++ dino_runner/game.c
@@ -1,20 +1,25 @@
-#include <stdint.h> // uint8_t etc...
-#include <stdlib.h> // srand
-#include <time.h> // time
+#include <stdint.h>  // uint8_t etc...
+#include <stdlib.h>  // srand
+#include <time.h>    // time
 #include <windows.h> // Sleep
 #include <stdbool.h> // boleanos
-#include <conio.h> // _kbhit() || _getch()
+#include <conio.h>   // _kbhit() || _getch()
 #include <stdio.h>
 #include <string.h> // strcpy e memset
 
 // TELA
 #define ALTURA 10
-#define LARGURA 70
+#define LARGURA 40
 #define ALTURA_MEIO (ALTURA / 2)
 #define LARGURA_MEIO (LARGURA / 2)
-#define DINO_INICIO 11
+
+// DINO
+#define DINO_INICIO 11      // coluna
+#define DINO_Y (ALTURA - 2) // altura
+#define DINO_PULO -2
+#define DINO_PULO_MAX (DINO_Y - 4) // 4 blocos de altura
 // MOB CACTO
-#define CACTO_INICIO  (LARGURA - 2)
+#define CACTO_INICIO (LARGURA - 2)
 #define CACTO_FIM (LARGURA - 68)
 #define MOB_CACTOS_SPAW 20
 
@@ -23,7 +28,11 @@ typedef struct
     int y;
     int x;
 
-    char *dinossauro;
+    // COMANDOS (w corre|| space pula)
+    bool estaPulando;
+    int velocidadeY;
+
+    char *dinossauro; // skin
 } Dino;
 Dino dino;
 
@@ -31,7 +40,6 @@ typedef struct
 {
     int y;
     int x;
-
     char *personagem;
 } Inimigos;
 
@@ -41,14 +49,16 @@ char tela[ALTURA][LARGURA][8];
 void inicializar()
 {
     SetConsoleOutputCP(CP_UTF8);
-    
+
     // Zera os bytes para nao ter lixo de memoria antes de usar o strcpy
     memset(tela, 0, sizeof(tela));
 
     // Preenche com espaço vazio usando string
-    for (int y = 0; y < ALTURA; y++) {
-        for (int x = 0; x < LARGURA; x++) {
-            strcpy(tela[y][x], " ");
+    for (int y = 0; y < ALTURA; y++)
+    {
+        for (int x = 0; x < LARGURA; x++)
+        {
+            strcpy(tela[y][x], "  ");
         }
     }
 
@@ -59,20 +69,20 @@ void inicializar()
         {
             for (int coluna_inteira = 0; coluna_inteira < LARGURA; coluna_inteira++)
             {
-                strcpy(tela[barreira_coluna][coluna_inteira], "#");
+                strcpy(tela[barreira_coluna][coluna_inteira], "##");
             }
         }
         else if (barreira_coluna == ALTURA - 1)
         {
             for (int coluna_inteira = 0; coluna_inteira < LARGURA; coluna_inteira++)
             {
-                strcpy(tela[barreira_coluna][coluna_inteira], "#");
+                strcpy(tela[barreira_coluna][coluna_inteira], "##");
             }
         }
         else
         {
-            strcpy(tela[barreira_coluna][0], "#");
-            strcpy(tela[barreira_coluna][LARGURA - 1], "#");
+            strcpy(tela[barreira_coluna][0], "##");
+            strcpy(tela[barreira_coluna][LARGURA - 1], "##");
         }
     }
 
@@ -93,18 +103,19 @@ void resetarCursor()
 }
 
 // APOIO 2
-void esconderCursor(bool desativar) {
+void esconderCursor(bool desativar)
+{
     HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO info;
     info.dwSize = 100;
-    
+
     if (desativar == true)
     {
-        info.bVisible = FALSE; 
+        info.bVisible = FALSE;
     }
     else
     {
-        info.bVisible = TRUE; 
+        info.bVisible = TRUE;
     }
     SetConsoleCursorInfo(consoleHandle, &info);
 }
@@ -122,8 +133,9 @@ void display()
     }
 }
 
-uint8_t sortearSimOuNao(void) {
-    return rand() % 2; 
+uint8_t sortearSimOuNao(void)
+{
+    return rand() % 2;
 }
 
 int main(void)
@@ -145,16 +157,19 @@ int main(void)
     {
         if (contador < MOB_CACTOS_SPAW)
         {
+
             contador++;
         }
 
-        // pula o dino
-        if (dino.y < ALTURA - 2)
+        if ((dino.estaPulando == false) && dino.y != DINO_Y)
         {
-            strcpy(tela[dino.y][dino.x], " ");
+            strcpy(tela[dino.y][dino.x], "  ");
             dino.y++;
             strcpy(tela[dino.y][dino.x], "🦖");
         }
+
+        // vai pra frente o dino
+
         if (dino.x > DINO_INICIO)
         {
             strcpy(tela[dino.y][dino.x], " ");
@@ -167,14 +182,14 @@ int main(void)
         {
             strcpy(tela[cacto.y][cacto.x], " ");
             cacto.x--;
-            
+
             if (cacto.x == CACTO_FIM + 1)
             {
                 cacto.x = CACTO_INICIO;
                 strcpy(tela[cacto.y][cacto.x], " ");
                 contador = 0;
             }
-            else 
+            else
             {
                 strcpy(tela[cacto.y][cacto.x], cacto.personagem);
             }
@@ -185,17 +200,18 @@ int main(void)
             if (_kbhit())
             {
                 char tecla = _getch();
-                if (tecla == ' ' || tecla == 32)
+                // PULO (Space)
+                if ((tecla == ' ' || tecla == 32) && dino.estaPulando == false)
                 {
-                    strcpy(tela[dino.y][dino.x], " ");
-                    dino.y = dino.y - 4;  
-                    strcpy(tela[dino.y][dino.x], "🦖");
+                    dino.velocidadeY = -2;
+                    dino.estaPulando = true;
                 }
+                // CORRER (W)
                 if (dino.x == DINO_INICIO)
                 {
                     if (tecla == 87 || tecla == 119) // teclas W e w
                     {
-                        strcpy(tela[dino.y][dino.x], " ");
+                        strcpy(tela[dino.y][dino.x], "  ");
                         dino.x = dino.x + 5;
                         strcpy(tela[dino.y][dino.x], "🦖");
                     }
@@ -208,16 +224,35 @@ int main(void)
             }
         }
 
+        // IFS DE CARACTERE APERTADO
+
+        // Aplica o movimento do pulo/gravidade
+        if (dino.estaPulando == true)
+        {
+            // APAGAR POSIÇÃO
+            if (dino.y == DINO_PULO_MAX)
+            {
+                // parar de subir
+                dino.estaPulando = false;
+            }
+            else
+            {
+                strcpy(tela[dino.y][dino.x], "  ");
+                dino.y = dino.y + DINO_PULO;
+                strcpy(tela[dino.y][dino.x], "🦖");
+            }
+        }
+
         // Enquanto existir qualquer tecla presa no buffer..
         while (_kbhit())
         {
-            _getch(); 
+            _getch();
         }
-        
+
         resetarCursor();
         display();
         esconderCursor(true);
-        Sleep(100); 
+        Sleep(100);
     }
     esconderCursor(false);
     return 0;
